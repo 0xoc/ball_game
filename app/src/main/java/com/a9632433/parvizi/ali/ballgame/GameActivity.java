accc5daca4af24fd1127255531b24343393127e8
Sina Parvizi <snparvizi75@gmail.com>
2019-01-24 01:04:53 +0330

    lost page

--- app/src/main/java/com/a9632433/parvizi/ali/ballgame/GameActivity.java
+++ app/src/main/java/com/a9632433/parvizi/ali/ballgame/GameActivity.java
@@ -2,28 +2,60 @@ package com.a9632433.parvizi.ali.ballgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.zip.Inflater;
 
 public class GameActivity extends AppCompatActivity {
+    LayoutInflater layoutInflater;
+
+    // current score
+    private int currentScore;
+
+    // all scores
+    private ArrayList<Integer> scores;
+
+    // indicates that the player lost was dealt with (due to un sync thread issues)
+    boolean lostDealtWith = false;
+
+    // root view group
+    ViewGroup root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        // main game root
+        root = findViewById(R.id.mainRoot);
+
+        // init layout inflater
+        layoutInflater = getLayoutInflater();
+
+
         createNewGameInstance(1);
     }
 
     public void createNewGameInstance(int level){
-        final FrameLayout gameContainer = findViewById(R.id.gameContainer);               // "game canvas"
-        final TextView score = findViewById(R.id.score);                                  // game score text view
-        final MainGameView game = new MainGameView(this,10,level);         // a game instance
+
+        // inflate the game layout
+        final View gameView = layoutInflater.inflate(R.layout.game_play_layout,null);
+
+        // add the game layout to the root layout
+        root.addView(gameView);
+
+        final FrameLayout gameContainer = gameView.findViewById(R.id.gameContainer);               // "game canvas"
+        final TextView score = gameView.findViewById(R.id.score);                                  // game score text view
+        final MainGameView game = new MainGameView(this,10,level);        // a game instance
 
 
         // add the game to the game container
@@ -34,6 +66,7 @@ public class GameActivity extends AppCompatActivity {
         checker.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
+
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -42,12 +75,18 @@ public class GameActivity extends AppCompatActivity {
                         score.setText(String.format("%d", game.getScore()));
 
                         if (game.isGameEnded()){    // the game is finished
-                            if (game.getFinalStatus() == false) {   // if player lost the game
+                            if (!game.getFinalStatus()) {   // if player lost the game
                                 // remove this game instance
-                                gameContainer.removeView(game);
+                                root.removeView(gameView);
+
+                                // set global game current game score
+                                currentScore = game.getScore();
 
                                 // cancel this timer
                                 checker.cancel();
+
+                                // show lost page
+                                createLostPage();
 
                                 // show high score list
                             } else {    // if player won the game
@@ -58,5 +97,20 @@ public class GameActivity extends AppCompatActivity {
                 });
             }
         },0,100);
+    }
+
+    public void createLostPage(){
+        if (lostDealtWith)  // page has been generated before
+            return;
+        lostDealtWith = true;
+
+        // inflate the lost page
+        View lostPage = layoutInflater.inflate(R.layout.game_lost_layout,null);
+        root.addView(lostPage);
+
+        // current score in lost page
+        TextView currentScoreText = lostPage.findViewById(R.id.currentScoreLost);
+        currentScoreText.setText(currentScore + "");
+
     }
 }
